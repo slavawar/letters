9c5c39c5ce833276335bbf42cfaddbf360734e56
Вячеслав Федоренко <slavafedora@yandex.ru>
2022-02-11 13:44:20 +0300

    edit to join str

--- alphabet.py
+++ alphabet.py
@@ -1,10 +1,10 @@
 dec_array = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-string = ''
 count = 1
+result_array = []
 
 for dec in dec_array:
     if count % 2 == 0: dec = dec.upper()
-    string = f'{string}{dec}[{str(count)}], '
+    result_array.append(f'{dec}[{str(count)}]')
     count = count + 1
 
-print(string)
+print(", ".join(result_array))
